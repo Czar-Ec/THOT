cc2163220f07be3b704fadd1d8f0de4cb28dd0a5
CzarEc <czar.echavez@yahoo.co.uk>
2019-05-27 10:28:46 +0100

    Initial commit

---
+++ src/main/java/io/github/czarec/thot/ThotApplication.java
@@ -0,0 +1,13 @@
+package io.github.czarec.thot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/czarec/thot/ThotApplicationTests.java
@@ -0,0 +1,16 @@
+package io.github.czarec.thot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ThotApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
